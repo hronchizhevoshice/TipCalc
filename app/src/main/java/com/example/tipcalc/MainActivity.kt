a45c889b50e0ae8f7c396385f9919157671f319d
hronchizhevoshice <hronchizhevoshice@gmail.com>
2025-10-06 14:39:59 +0700

    Add two fields

--- app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,6 +47,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Order(modifier: Modifier = Modifier)
 {
+    var orderAmount by remember { mutableStateOf("") }
+    var Count by remember { mutableStateOf("") }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
@@ -50,6 +62,25 @@ fun Order(modifier: Modifier = Modifier)
                 fontSize = 15.sp,
                 modifier = Modifier.padding(end = 8.dp)
             )
+            Box(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(20.dp)
+                    .background(Color(0xFFFFC0CB))
+                    .padding(horizontal = 4.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+            androidx.compose.foundation.text.BasicTextField(
+                modifier = Modifier
+                    .width(150.dp),
+                value = orderAmount,
+                onValueChange = { orderAmount = it },
+                singleLine = true,
+                textStyle = androidx.compose.ui.text.TextStyle(
+                    fontSize = 14.sp,
+                    color = Color.Black)
+                )
+            }
         }
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text(
@@ -57,6 +88,25 @@ fun Order(modifier: Modifier = Modifier)
                 fontSize = 15.sp,
                 modifier = Modifier.padding(end = 8.dp)
             )
+            Box(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(20.dp)
+                    .background(Color(0xFFFFC0CB))
+                    .padding(horizontal = 4.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                androidx.compose.foundation.text.BasicTextField(
+                    modifier = Modifier
+                        .width(50.dp),
+                    value = Count,
+                    onValueChange = { Count = it },
+                    singleLine = true,
+                    textStyle = androidx.compose.ui.text.TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.Black)
+                )
+            }
         }
     }
 }
