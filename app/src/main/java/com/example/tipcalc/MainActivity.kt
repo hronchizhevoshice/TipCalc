27275266f7f32914b89c68d3101ab2f836963f9f
hronchizhevoshice <hronchizhevoshice@gmail.com>
2025-10-06 14:31:31 +0700

    Add text

--- app/src/main/java/com/example/tipcalc/MainActivity.kt
+++ app/src/main/java/com/example/tipcalc/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tipcalc.ui.theme.TipCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TipCalcTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Order(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Order(modifier: Modifier = Modifier)
+{
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
-    )
+            .padding(top = 50.dp)
+            .padding(horizontal = 20.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Сумма заказа:",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Количество блюд:",
+                fontSize = 15.sp,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun OrderPreview()
+{
     TipCalcTheme {
-        Greeting("Android")
+        Order()
     }
 }
